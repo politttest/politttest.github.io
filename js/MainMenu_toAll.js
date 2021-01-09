118d5a73a7d027b96a92c54e02c25bbc3df78956
politttest <77047995+politttest@users.noreply.github.com>
2021-01-09 23:56:35 +0200

    Add files via upload

---
+++ js/MainMenu_toAll.js
@@ -0,0 +1,7 @@
+function to_tests(){
+    document.location.href = "tests.html";
+}
+
+function to_create_test(){
+    document.location.href = "create_test.html"
+}
--- js/infa.js
+++ js/infa.js
@@ -2,3 +2,11 @@
 document.getElementById('img_google').url = localStorage.getItem('photoURL_google');
 document.getElementById('email_google').textContent = localStorage.getItem('email_google');
 document.getElementById('name_google').textContent = localStorage.getItem('displayName_google');
+
+function to_tests(){
+    document.location.href = "tests.html";
+}
+
+function to_create_test(){
+    document.location.href = "create_test.html"
+}
