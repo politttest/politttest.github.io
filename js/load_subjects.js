c6e0b362b23bc6db258589a5b541220ca2641a01
politttest <77047995+politttest@users.noreply.github.com>
2021-02-08 23:48:37 +0200

    Add files via upload

--- js/load_subjects.js
+++ js/load_subjects.js
@@ -4,10 +4,10 @@ var value, id_of_the_test, klass_of_the_test, name_of_the_test, subject_of_the_t
 databaseRef.orderByKey().on('value', snapshot => {
     snapshot.forEach(function (childSnapshot) {
         value = childSnapshot.val();
-        console.log(value);
+        //console.log(value);
         var selected = document.getElementById("subject");
         var op = document.createElement("option");
         op.innerHTML = value;
         selected.appendChild(op);
     })})
-console.log("adwad");
+//console.log("adwad");
--- js/save_test.js
+++ js/save_test.js
@@ -22,22 +22,22 @@ var klss, klss_text, sbjct, sbjct_text, tm_text, nm_text;
 
           function save_the_test() {
               output_information_about_klass();
-              //var psh =  firebase.database().ref().child('/tests/').push();
+              var psh =  firebase.database().ref().child('/tests/').push();
               //document.write(firebase.database().ref().child('/tests/').push().key);
               if (nm_text == "") {
               window.alert("поле: 'Назва теста' має бути заповненим!"); 
               return;
             }
-              /*psh.update({
+              psh.update({
               c1ass: klss_text,
               title: nm_text,
               subject: sbjct_text,
               time: tm_text,
               id: psh.key,
               //console.log(nm_text)
-          })*/
-              console.log(tm_text_v);
-              console.log(tm_text);
+          })
+              //console.log(tm_text_v);
+              //console.log(tm_text);
               document.getElementById("save_button").style.backgroundColor = "darkgrey";
               document.getElementById("save_button").onclick = "";
               window.alert("Тест було створено. Щоб добавити питання/учнів або редагувати тест - натисніть на кнопку 'Тести' на головному меню.");
