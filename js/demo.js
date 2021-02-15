1774642c7cd5da6fc773b8cd55d22bb76be00d2a
politttest <77047995+politttest@users.noreply.github.com>
2021-02-15 23:58:07 +0200

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,8 @@
+function child(){
+    document.getElementById("choice").className = "none"
+    document.getElementById("child__auth").className = "block"
+}
+function child__connect(){
+    document.getElementById("child__auth").className = "none"
+    document.getElementById("child__main_menu").className = "block"
+}
