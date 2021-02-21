b44c319dc5a9d92d998a2a91f3fd91d6fbad3ff9
politttest <77047995+politttest@users.noreply.github.com>
2021-02-21 23:33:16 +0200

    Add files via upload

--- js/demo.js
+++ js/demo.js
@@ -1,14 +1,30 @@
+function Go_To_PlayMarket(){
+    document.location.href = "https://play.google.com/store/apps/details?id=com.m21droid.politest"
+}
+
+
 function clear_answer_in_the_write_test() {
     document.forms.test_answer.reset();
+}
+function off_all(){
+    document.getElementById("child__auth").className = "none"
+    document.getElementById("child__main_menu").className = "none"
+    document.getElementById("child__tests").className = "none"
+    document.getElementById("child__write_test").className = "none"
+    document.getElementById("teacher__auth").className = "none"
+    document.getElementById("teacher__main_menu").className = "none"
+    document.getElementById("teacher__list_with_questions").className = "none"
+    document.getElementById("teacher__to_list_with_test").className = "none"
+    document.getElementById("teacher__update_test").className = "none"
+    document.getElementById("teacher__answer").className = "none"
+    document.getElementById("teacher__list_with_testers").className = "none"
+    document.getElementById("teacher__list_with_results").className = "none"
 }
 function Restart(){
     setTimeout(() => {
         clear_answer_in_the_write_test();
         document.getElementById("choice").className = "block"
-        document.getElementById("child__auth").className = "none"
-        document.getElementById("child__main_menu").className = "none"
-        document.getElementById("child__tests").className = "none"
-        document.getElementById("child__write_test").className = "none"
+        off_all();
     }, 200);
 }
 function child(){
@@ -83,7 +99,55 @@ function teacher__test_banned(){
         x.className = x.className.replace("show", "");
     }, 3000);
 }
+function teacher__answer_banned(){
+    var x = document.getElementById("snackbar_forth");
+    x.className = "show";
+    setTimeout(() => {
+        x.className = x.className.replace("show", "");
+    }, 3000);
+}
+function delete_test(){
+    var x = document.getElementById("snackbar_fifth");
+    x.className = "show";
+    setTimeout(() => {
+        x.className = x.className.replace("show", "");
+    }, 3000);
+}
+function teacher__to_list_with_update(){
+    delete_test(); //так как вызывается один и тот же элемент, я объединил
+}
 function teacher__update_test(){
     document.getElementById("teacher__to_list_with_test").className = "none"
     document.getElementById("teacher__update_test").className = "block"
+    document.getElementById("teacher__list_with_testers").className = "none"
+}
+function teacher__to_list_with_questions(){
+    document.getElementById("teacher__update_test").className = "none"
+    document.getElementById("teacher__list_with_questions").className = "block"
+}
+function teacher__to_answer(){
+    document.getElementById("teacher__list_with_questions").className = "none"
+    document.getElementById("teacher__answer").className = "block"
+}
+function teacher__to_list_with_testers(){
+    document.getElementById("teacher__update_test").className = "none"
+    document.getElementById("teacher__list_with_testers").className = "block"
+}
+function teacher__to_list_with_marks(){
+    document.getElementById("teacher__update_test").className = "none"
+    document.getElementById("teacher__list_with_results").className = "block"
+}
+function teacher__button_question(){
+    var x = document.getElementById("snackbar_sixth");
+    x.className = "show";
+    setTimeout(() => {
+        x.className = x.className.replace("show", "");
+    }, 3000);
+}
+function teacher__button_question2(){
+    var x = document.getElementById("snackbar_seventh");
+    x.className = "show";
+    setTimeout(() => {
+        x.className = x.className.replace("show", "");
+    }, 3000);
 }
